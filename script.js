be352b1fa639354d00cb2f446a6a3e420bdcb7f3
Mikhail Shvets <shvetsm123@gmail.com>
2023-11-10 22:58:49 +0200

    maybe final commit

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toggleBurgerMenu() {
-    var burgerMenu = document.getElementById("burgerMenu");
+    let burgerMenu = document.getElementById("burgerMenu");
+    let mainNav = document.getElementById("mainNav");
     if (burgerMenu.style.display === "block") {
         burgerMenu.style.display = "none";
     } else {
         burgerMenu.style.display = "block";
+        mainNav.style.display = "flex";
     }
 }
