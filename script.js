a3c447f37819cf4bb13498e2a9e1dfaa1a0f548d
Mikhail Shvets <shvetsm123@gmail.com>
2023-11-10 17:49:17 +0200

    minor try

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleBurgerMenu() {
+    var burgerMenu = document.getElementById("burgerMenu");
+    if (burgerMenu.style.display === "block") {
+        burgerMenu.style.display = "none";
+    } else {
+        burgerMenu.style.display = "block";
+    }
+}
