f20b2fe672481f0ea1385e3078f132c0a45bf6f7
Mikhail Shvets <shvetsm123@gmail.com>
2023-12-26 22:30:48 +0200

    changes

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function toggleBurgerMenu() {
         mainNav.style.display = "flex";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var checkboxes = document.querySelectorAll(".accordion-checkbox");
+
+    checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener("change", function () {
+            var group = checkbox.getAttribute("data-group");
+
+            checkboxes.forEach(function (otherCheckbox) {
+                if (
+                    otherCheckbox !== checkbox &&
+                    otherCheckbox.getAttribute("data-group") === group
+                ) {
+                    otherCheckbox.checked = false;
+                }
+            });
+        });
+    });
+});
